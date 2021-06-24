db02827e32618893933c00aacca8582d26afd0fc
pyrhaa <sboubtane@protonmail.com>
2021-06-24 21:59:58 +0200

    always in progress

--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
+//a comment just for commit
+
 const Review = () => {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index];
