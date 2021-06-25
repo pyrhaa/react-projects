f71f7ddfc8d2430d9bd3c6816aa2b7df1129e5d6
pyrhaa <sboubtane@protonmail.com>
2021-06-25 11:38:50 +0200

    review.js apparence ok

--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -2,8 +2,6 @@ import React, { useState } from 'react';
 import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
-//a comment just for commit
-
 const Review = () => {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index];
@@ -11,7 +9,22 @@ const Review = () => {
     <article className='review'>
       <div className='img-container'>
         <img src={image} alt={name} className='person-img' />
+        <span className='quote-icon'>
+          <FaQuoteRight />
+        </span>
       </div>
+      <h4 className='author'>{name}</h4>
+      <p className='job'>{job}</p>
+      <p className='info'>{text}</p>
+      <div className='button-container'>
+        <button className='prev-btn'>
+          <FaChevronLeft />
+        </button>
+        <button className='next-btn'>
+          <FaChevronRight />
+        </button>
+      </div>
+      <button className='random-btn'>suprise me</button>
     </article>
   );
 };
