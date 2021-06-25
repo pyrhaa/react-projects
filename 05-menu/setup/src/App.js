035582e24d4bbb69aadcd53b87ddbdfac5487f5a
pyrhaa <sboubtane@protonmail.com>
2021-06-25 18:36:31 +0200

    set categories in progress

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -3,9 +3,21 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+//new Set ; it's for remove duplicate elements from the array
+const allCategories =['all', ... new Set(items.map(item => item.category))]
+
 function App() {
   const [menuItems,setMenuItems] = useState(items);
   const [categories, setCategories] = useState([]);
+
+  const filterItems = (category) => {
+    if(category === 'all'){
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter(item => item.category === category)
+    setMenuItems(newItems)
+  }
 
   return <main>
     <section className="menu section">
@@ -13,7 +25,7 @@ function App() {
         <h2>our menu</h2>
         <div className="underline"></div>
       </div>
-      <Categories />
+      <Categories filterItems={filterItems} />
       <Menu items={menuItems} />
     </section>
   </main>
