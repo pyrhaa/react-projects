5f5e5e01080283fb19f1d5a63ca3956acfc53d22
pyrhaa <sboubtane@protonmail.com>
2021-06-30 18:37:42 +0200

    title style ok

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -4,7 +4,16 @@ import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
 function App() {
   const [people, setPeople] = useState(data);
-  return <h2>reviews</h2>;
+  const [index, setIndex] = useState(0);
+  return (
+    <section className='section'>
+      <div className='title'>
+        <h2>
+          <span>/</span>reviews
+        </h2>
+      </div>
+    </section>
+  );
 }
 
 export default App;
