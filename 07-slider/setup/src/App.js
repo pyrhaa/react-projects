4dfe1d865a54be3755849ac70444645850e0bf90
pyrhaa <sboubtane@protonmail.com>
2021-06-30 18:53:01 +0200

    button displayed

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -12,6 +12,27 @@ function App() {
           <span>/</span>reviews
         </h2>
       </div>
+      <div className='section-center'>
+        {people.map((person, personIndex) => {
+          const { id, image, name, title, quote } = person;
+
+          return (
+            <article key={id}>
+              <img src={image} alt={name} className='person-img' />
+              <h4>{name}</h4>
+              <p className='title'>{title}</p>
+              <p className='text'>{quote}</p>
+              <FaQuoteRight className='icon' />
+            </article>
+          );
+        })}
+        <button className='prev'>
+          <FiChevronLeft />
+        </button>
+        <button className='next'>
+          <FiChevronRight />
+        </button>
+      </div>
     </section>
   );
 }
