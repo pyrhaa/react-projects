15fae139f74283a7d1ed89daf322516b89a40d8b
pyrhaa <sboubtane@protonmail.com>
2021-06-30 19:42:01 +0200

    slider end

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -5,6 +5,22 @@ import data from './data';
 function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+    if (index > lastIndex) {
+      setIndex(0);
+    }
+  }, [index, people]);
+
+  useEffect(() => {
+    let slider = setInterval(() => setIndex(index + 1), 3000);
+    return () => clearInterval(slider);
+  }, [index]);
+
   return (
     <section className='section'>
       <div className='title'>
@@ -19,6 +35,12 @@ function App() {
           if (personIndex === index) {
             position = 'activeSlide';
           }
+          if (
+            personIndex === index - 1 ||
+            (index === 0 && personIndex === people.length - 1)
+          ) {
+            position = 'lastSlide';
+          }
           return (
             <article className={position} key={id}>
               <img src={image} alt={name} className='person-img' />
@@ -29,10 +51,10 @@ function App() {
             </article>
           );
         })}
-        <button className='prev'>
+        <button className='prev' onClick={() => setIndex(index - 1)}>
           <FiChevronLeft />
         </button>
-        <button className='next'>
+        <button className='next' onClick={() => setIndex(index + 1)}>
           <FiChevronRight />
         </button>
       </div>
