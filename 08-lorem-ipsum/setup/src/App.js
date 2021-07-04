c11ee4ca57d6c747d96c705d9ab793725aa7abc9
pyrhaa <sboubtane@protonmail.com>
2021-07-04 20:09:08 +0200

    lorem  not finish yet

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -7,8 +7,13 @@ function App() {
   const handleSubmit = (e) => {
     e.preventDefault();
     let amount = parseInt(count);
-
-    setText(data);
+    if (count <= 0) {
+      amount = 1;
+    }
+    if (count > 8) {
+      amount = 8;
+    }
+    setText(data.slice(0, amount));
   };
 
   return (
