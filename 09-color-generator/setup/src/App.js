4746d56ed3f1a730dbb9b9f03f78e88a43add68e
pyrhaa <sboubtane@protonmail.com>
2021-07-06 16:15:36 +0200

    app singleColor utils update in ES6 function syntax

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -10,7 +10,13 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('azul');
+    try {
+      let colors = new Values(color).all(10)
+      setList(colors)
+    } catch (error) {
+      setError(true)
+    }
+    
   }
 
   return (
@@ -18,14 +24,16 @@ function App() {
   <section className='container'>
     <h3>color generator</h3>
     <form onSubmit={handleSubmit}>
-      <input type="text" value={color} onChange ={(e) => setColor(e.target.value)} placeholder='#f15025' />
+      <input type="text" value={color} onChange ={(e) => setColor(e.target.value)} placeholder='#f15025' className={`${error ? 'error' : null}`} />
       <button className='btn' type='submit'>
         submit
       </button>
     </form>
   </section>
   <section className='colors'>
-    <h4>list goes here</h4>
+    {list.map((color, index) => {
+      return <SingleColor />
+    })}
   </section>
   </>
   )
