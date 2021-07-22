af5a7a65a5cf4f998472fb24462cddba3b3514a8
pyrhaa <sboubtane@protonmail.com>
2021-07-22 21:52:59 +0200

    put form in section

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -1,22 +1,23 @@
-import React, { useState, useEffect } from 'react'
-import List from './List'
-import Alert from './Alert'
+import React, { useState, useEffect } from 'react';
+import List from './List';
+import Alert from './Alert';
 
 function App() {
   const [name, setName] = useState('');
   const [list, setList] = useState([]);
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null);
-  const [alert, setAlert] = useState({show: false, msg: '', type: ''});
+  const [alert, setAlert] = useState({ show: false, msg: '', type: '' });
 
-  return <section className="section-center">
-    <div className="grocery-container">
-      <List />
-      <button className='clear-btn'>
-        clear items
-      </button>
-    </div>
-  </section>
+  return (
+    <section className='section-center'>
+      <form className='grocery-form'></form>
+      <div className='grocery-container'>
+        <List />
+        <button className='clear-btn'>clear items</button>
+      </div>
+    </section>
+  );
 }
 
-export default App
+export default App;
