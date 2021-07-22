c50a0efa3b20a3e8902f39a560e2d6f8b7229f65
pyrhaa <sboubtane@protonmail.com>
2021-07-22 21:57:00 +0200

    handleSubmit ok

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -9,9 +9,14 @@ function App() {
   const [editID, setEditID] = useState(null);
   const [alert, setAlert] = useState({ show: false, msg: '', type: '' });
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('hello');
+  };
+
   return (
     <section className='section-center'>
-      <form className='grocery-form'></form>
+      <form className='grocery-form' onSubmit={handleSubmit}></form>
       <div className='grocery-container'>
         <List />
         <button className='clear-btn'>clear items</button>
