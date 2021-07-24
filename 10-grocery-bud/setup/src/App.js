30594ee823732cbeb8dd91501e7e8de2bf52bfd0
pyrhaa <sboubtane@protonmail.com>
2021-07-24 12:30:31 +0200

    delete item onclick ok

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -35,6 +35,11 @@ function App() {
     setList([]);
   };
 
+  const removeItem = (id) => {
+    showAlert(true, 'danger', 'item removed');
+    setList(list.filter((item) => item.id !== id));
+  };
+
   return (
     <section className='section-center'>
       <form className='grocery-form' onSubmit={handleSubmit}>
@@ -55,7 +60,7 @@ function App() {
       </form>
       {list.length > 0 && (
         <div className='grocery-container'>
-          <List items={list} />
+          <List items={list} removeItem={removeItem} />
           <button className='clear-btn' onClick={clearList}>
             clear items
           </button>
