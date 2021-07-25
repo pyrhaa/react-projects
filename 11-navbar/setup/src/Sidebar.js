e10695927ece1408041215fe7dd88e57c4293967
pyrhaa <sboubtane@protonmail.com>
2021-07-25 11:36:18 +0200

    icons from data put in navbar

--- 11-navbar/setup/src/Sidebar.js
+++ 11-navbar/setup/src/Sidebar.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import { links, social } from './data';
 
+// est modulaire car quand data est modifié, cela modifie navbar et sidebar ci-dessous.
 const Sidebar = () => {
   return (
-    <div className='links-container show-container'>
-      <ul className='links'>
-        {links.map((link) => {
-          const { id, url, text } = link;
-          return (
-            <li key={id}>
-              <a href={url}>{text}</a>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+    <>
+      <h4>Sidebar</h4>
+      <div className='links-container show-container'>
+        <ul className='links'>
+          {links.map((link) => {
+            const { id, url, text } = link;
+            return (
+              <li key={id}>
+                <a href={url}>{text}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </>
   );
 };
 
