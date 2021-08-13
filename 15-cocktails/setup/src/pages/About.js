f076566e970442a81b4e1292dd4cc958c5544e7f
pyrhaa <sboubtane@protonmail.com>
2021-08-13 10:54:07 +0200

    about.js ok

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam
+        quod ipsa architecto. Labore commodi voluptatum iste vel, expedita
+        corporis quas molestiae excepturi consequatur, neque voluptate nesciunt,
+        saepe mollitia fugit architecto nulla. Doloribus maxime iste eius
+        similique harum vero nobis dolores?
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
