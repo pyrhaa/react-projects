ca244dcf86331596833c068bd75006ca1d3f792c
pyrhaa <sboubtane@protonmail.com>
2021-08-14 14:32:22 +0200

    handlesubmit display

--- 15-cocktails/setup/src/components/SearchForm.js
+++ 15-cocktails/setup/src/components/SearchForm.js
@@ -4,13 +4,22 @@ import { useGlobalContext } from '../context';
 const SearchForm = () => {
   const { setSearchTerm } = useGlobalContext();
   const searchValue = React.useRef('');
+
+  React.useEffect(() => {
+    searchValue.current.focus();
+  }, []);
+
   const searchCocktail = () => {
     setSearchTerm(searchValue.current.value);
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <section className='section search'>
-      <form action='' className='search-form'>
+      <form action='' className='search-form' onSubmit={handleSubmit}>
         <div className='form-control'>
           <label htmlFor='name'>search your favorite cocktail</label>
           <input
